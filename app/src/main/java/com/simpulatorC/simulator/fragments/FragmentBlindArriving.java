47f8345a9024a459b583ad4b3861b82d362b9f4c
GoodDamn <76108467+GoodDamn@users.noreply.github.com>
2021-04-16 23:35:52 +0300

    Added list of commands and fixed some bugs

--- app/src/main/java/com/simpulatorC/simulator/fragments/FragmentBlindArriving.java
+++ app/src/main/java/com/simpulatorC/simulator/fragments/FragmentBlindArriving.java
@@ -1,6 +1,9 @@
 package com.simpulatorC.simulator.fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
@@ -13,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,24 +27,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.ListAdapter;
 
 import com.simpulatorC.simulator.FileStream;
 import com.simpulatorC.simulator.R;
 import com.simpulatorC.simulator.activities.ActivitySupport;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 public class FragmentBlindArriving extends Fragment {
 
     private static String STATE_FILE = "State",
         TIME_FILE = "Time",
         DIRECTION_FILE = "Direction",
-        METERS_FILE = "Meters";
+        METERS_FILE = "Meters",
+        COMMANDS_FILE = "Commands";
 
     private EditText commandLine;
     private Button turnFlashlights;
@@ -57,7 +60,7 @@ public class FragmentBlindArriving extends Fragment {
 
     private void ShowToastMessage(String text) { Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();}
 
-    private void ChangeState(String stateName, int meters, String direction)
+    private void ChangeState(String stateName, long meters, String direction)
     {
         FileStream fileStream = new FileStream();
         try
@@ -97,7 +100,7 @@ public class FragmentBlindArriving extends Fragment {
         turnFlashlights.setTextColor(textColour);
     }
 
-    private void Move(String direction, final ImageButton button, int meters, long delay) // MOVE
+    private void Move(String direction, final ImageButton button, long meters, long delay) // MOVE
     {
         if (!isMoving)
         {
@@ -139,7 +142,7 @@ public class FragmentBlindArriving extends Fragment {
         commandLine.setEnabled(enabled);
     }
 
-    private int getNumberFromCommand(String command) // Get number, if command has numbers
+    private long getNumberFromCommand(String command) // Get number, if command has numbers
     {
         String number = "";
         for (char ch : command.toCharArray()) {
@@ -148,7 +151,7 @@ public class FragmentBlindArriving extends Fragment {
                 ch == '7' || ch=='8' || ch=='9' || ch=='0')
                 number += ch;
         }
-        return Integer.parseInt(number);
+        return Long.parseLong(number);
     }
 
     private String getCommand(String command) // Get command name from command line with numbers
@@ -167,7 +170,7 @@ public class FragmentBlindArriving extends Fragment {
     private void ExecuteCommand(String command) //Method, which execute command from command line
     {
         // String to lower case and remove whitespaces.
-        int numSteps = 0;
+        long numSteps = 0;
         if ((command.indexOf("moveforward") != -1) || (command.indexOf("moveback") != -1) ||
                 (command.indexOf("moveright") != -1) || (command.indexOf("moveleft") != -1)) {
             numSteps = getNumberFromCommand(command);
@@ -184,7 +187,7 @@ public class FragmentBlindArriving extends Fragment {
             TurnOnOffFlashlight(Color.WHITE, getString(R.string.turn_off_flashlights),
                     R.drawable.style_button_rounded_black);
             break;
-            case "sleep":
+            case "sleep": // Command - Sleep
                 SleepMode(false, fadeIn_state, getString(R.string.robot_sleep), R.drawable.style_button_rounded_blue);
                 break; case "moveforward": Move(getString(R.string.moveForward), moveForward, numSteps, 750 * numSteps);
                 break; // Move forward
@@ -216,8 +219,17 @@ public class FragmentBlindArriving extends Fragment {
         moveLeft = v.findViewById(R.id.Ibutton_move_Left); // Find button with left arrow
         moveRight = v.findViewById(R.id.Ibutton_move_right); // Find button with right arrow
         stack_of_commands = v.findViewById(R.id.listView_stack_of_commands); // Find ListView
-        adapter_commands = new ArrayAdapter<>(getContext(), R.layout.support_simple_spinner_dropdown_item);
+        adapter_commands = new ArrayAdapter<>(getContext(), R.layout.list_view_row); // Initialize adapter for listView
 
+        stack_of_commands.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                DialogRemoveItem dialogRemoveItem = new DialogRemoveItem(adapter_commands.getItem(i), adapter_commands);
+                dialogRemoveItem.show(getFragmentManager(), "dialog");
+            }
+        });
+
+        //Initialize Animations
         fadeIn_state = AnimationUtils.loadAnimation(getContext(),R.anim.fadein); // Initialize fade in animation for state
         fadeOut_state = AnimationUtils.loadAnimation(getContext(), R.anim.fadeout); // Initialize fade out animation for state
         fadeIn_state.setAnimationListener(new Animation.AnimationListener() {
@@ -265,18 +277,20 @@ public class FragmentBlindArriving extends Fragment {
             }
         });
 
-        ImageView imageView = v.findViewById(R.id.ImageView_commands);
+        ImageView imageView = v.findViewById(R.id.ImageView_commands); // Button, which is started ActivitySupport class.
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view) { // Listener
                 Intent intent = new Intent(getContext(), ActivitySupport.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 getActivity().finish();
                 getActivity().overridePendingTransition(R.anim.enter_from_top_to_bottom, R.anim.exit_from_top_to_bottom);
                 startActivity(intent);
+                SaveState();
             }
         });
 
+        // Button "Move Forward"
         moveForward.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
                 if (moveForward.getBackground().getConstantState().equals(getDrawable(R.drawable.moving_active).getConstantState()))
@@ -285,12 +299,14 @@ public class FragmentBlindArriving extends Fragment {
             }
         });
 
+        // Button "Move back"
         moveBack.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
                 if (moveBack.getBackground().getConstantState().equals(getDrawable(R.drawable.moving_active).getConstantState()))
                     ReturnToGeneralState();
                 else Move(getString(R.string.moveBack), moveBack, 2,1500);
             }});
+        // Button "Move right"
         moveRight.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
                 if (moveRight.getBackground().getConstantState().equals(getDrawable(R.drawable.moving_active).getConstantState()))
@@ -298,6 +314,7 @@ public class FragmentBlindArriving extends Fragment {
                 else Move(getString(R.string.moveRight), moveRight, 2,1500);
             }
         });
+        // Button "Move left"
         moveLeft.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
                 if (moveLeft.getBackground().getConstantState().equals(getDrawable(R.drawable.moving_active).getConstantState()))
@@ -306,10 +323,11 @@ public class FragmentBlindArriving extends Fragment {
             }
         });
 
-        commandLine = v.findViewById(R.id.edittext_commandLine);
+        commandLine = v.findViewById(R.id.edittext_commandLine); // EditText "Command line"
         commandLine.setOnEditorActionListener(new TextView.OnEditorActionListener() { // Action, when user clicked "Enter" on soft keyboard.
             @Override
-            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) { /* Listen, if user pressed on button
+            on soft keyboard and focused on EditText "Command Line".*/
                 String command = commandLine.getText().toString().toLowerCase().replaceAll("\\s+", "");
                 if (isMoving)
                 {
@@ -345,22 +363,33 @@ public class FragmentBlindArriving extends Fragment {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        if (text != null)
+        if (text != null) // If file not empty
         {
-            switch (text)
+            switch (text) // Select current state
             {
-                case "Sleep":
+                case "Sleep": // If robot was sleeping last time
                     SleepMode(false, fadeIn_state, "Robot is sleeping", R.drawable.style_button_rounded_blue);
                     break;
-                case "Move":
+                case "Move": // If robot was moving last time.
                     try
                     {
                         FileStream fileStream = new FileStream();
+                        String commands = fileStream.ReadFile(getActivity().openFileInput(COMMANDS_FILE)), // Open file with saving commands.
+                            com = "";
+                        for (char ch : commands.toCharArray()) // Analyze data from file.
+                        {
+                            if (ch != ' ') com += ch;
+                            else {
+                                Log.d("123456", com);
+                                adapter_commands.add(com); // Add Command to listView
+                                com = "";
+                            }
+                        }
+                        stack_of_commands.setAdapter(adapter_commands); // Show commands in listView.
                         long previousTime = Long.parseLong(fileStream.ReadFile(getActivity().openFileInput(TIME_FILE))),
                             currentTime = Calendar.getInstance().getTimeInMillis();
                         int meters = Integer.parseInt(fileStream.ReadFile(getActivity().openFileInput(METERS_FILE)));
                         long currentDelay = 750 * meters - (currentTime - previousTime);
-                        Log.d("123456", fileStream.ReadFile(getActivity().openFileInput(DIRECTION_FILE)));
                         switch (fileStream.ReadFile(getActivity().openFileInput(DIRECTION_FILE)))
                         {
                             case "forward":
@@ -387,16 +416,53 @@ public class FragmentBlindArriving extends Fragment {
     }
 
 
-    public void SaveState()
+    public void SaveState() // Method, which save current state of robot in files.
     {
-        if (isMoving)
+        if (isMoving) // if robot is moving
         {
-            try { // Save time, if user quited from app.
-                new FileStream().SaveFile(String.valueOf(Calendar.getInstance().getTimeInMillis()), getActivity().openFileOutput(TIME_FILE, Context.MODE_PRIVATE));
+            FileStream fileStream = new FileStream();
+            try { // Save time and stack of commands, if user quited from app.
+                fileStream.SaveFile(String.valueOf(Calendar.getInstance().getTimeInMillis()), getActivity().openFileOutput(TIME_FILE, Context.MODE_PRIVATE));
+                String strCommands = "";
+                for (int i = 0; i < adapter_commands.getCount(); i++)
+                {
+                    strCommands += adapter_commands.getItem(i);
+                    strCommands += " ";
+                }
+                Log.d("123456", strCommands);
+                fileStream.SaveFile(strCommands, getActivity().openFileOutput(COMMANDS_FILE, Context.MODE_PRIVATE));
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
         }
     }
 
+
+    // Call Dialog window, when the user wants to remove command from list.
+    public static class DialogRemoveItem extends AppCompatDialogFragment {
+
+        private String commandName;
+        private ArrayAdapter<String> arrayAdapter;
+
+        public DialogRemoveItem(String commandName, ArrayAdapter<String> arrayAdapter)
+        {
+            this.commandName = commandName;
+            this.arrayAdapter = arrayAdapter;
+        }
+
+        @NonNull @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle(getString(R.string.removeItem))
+                    .setMessage(getString(R.string.removeCommand) + " (" + commandName + ") ?")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() { // If clicked "OK".
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            arrayAdapter.remove(commandName);
+                        }
+                    })
+                    .setNegativeButton(getString(R.string.cancel), null);
+            return builder.create();
+        }
+    }
 }
