885f728f4f03b36b258a5f63ae897a79d5e4e43e
GoodDamn <76108467+GoodDamn@users.noreply.github.com>
2021-04-20 21:16:11 +0300

    Splash screen

---
+++ app/src/main/java/com/simpulatorC/simulator/activities/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.simpulatorC.simulator.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.simpulatorC.simulator.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                finish();
+                startActivity(intent);
+                overridePendingTransition(R.anim.enter_from_top_to_bottom, R.anim.exit_from_top_to_bottom);
+            }
+        }, 2150);
+
+    }
+}
